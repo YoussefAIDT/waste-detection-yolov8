71a02bf5ca6f66b420a65475a19d7d40d3f2ba15
zakariazemmahi <zakariaezemmahi@gmail.com>
2025-05-21 12:51:55 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,11 @@
-# Configuration de la documentation Sphinx pour Read the Docs
-
 import os
 import sys
-
-# Ajouter le dossier parent au chemin système
 sys.path.insert(0, os.path.abspath('..'))
 
-# Informations sur le projet
 project = 'Waste Detection YOLOv8'
 author = 'Zakariae Zemmahi / ES-SAAIDI Youssef / HAJJI Mohamed'
 release = '1.0'
 
-# Extensions utilisées par Sphinx
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -19,32 +13,15 @@ extensions = [
     'sphinx.ext.todo'
 ]
 
-# Chemins vers les templates
 templates_path = ['_templates']
-
-# Fichiers à exclure
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# Thème HTML
 html_theme = 'sphinx_rtd_theme'
-
-# Fichiers statiques (CSS personnalisé, images, etc.)
 html_static_path = ['_static']
+html_css_files = ['custom_readthedocs_css.css']
 
-# Ajouter un fichier CSS personnalisé
-html_css_files = [
-    'custom_readthedocs_css.css'
-]
-
-# Options de thème (optionnel)
 html_theme_options = {
     'navigation_depth': 3,
     'collapse_navigation': False,
     'sticky_navigation': True,
 }
-
-# Logo personnalisé (optionnel)
-# html_logo = '_static/logo.png'
-
-# Favicon (optionnel)
-# html_favicon = '_static/favicon.ico'
