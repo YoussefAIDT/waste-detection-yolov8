8ddc732eacaa7c4372a876d5a95fc16d479519bf
zakariazemmahi <zakariaezemmahi@gmail.com>
2025-06-05 20:53:15 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,26 +2,15 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'Waste Detection YOLOv8'
+project = 'Smart Waste Detection'
+copyright = '2025, Ayoub Zemmahi'
 author = 'Zakariae Zemmahi / ES-SAAIDI Youssef / HAJJI Mohamed'
 release = '1.0'
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo'
-]
+extensions = ['recommonmark', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
-html_css_files = ['custom_readthedocs_css.css']
-
-html_theme_options = {
-    'navigation_depth': 3,
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-}
